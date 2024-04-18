272444fbff16b8331beeba20ce3c171c131173bc
정승현 <armyso9506@plateer.com>
2024-04-18 13:22:56 +0900

    [ X2BEE ] Project init
    Nuxt3 신규 프로젝트 생성

--- x2bee/nuxt.config.ts
+++ x2bee/nuxt.config.ts
@@ -1,20 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-// @ts-ignore
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  typescript: {
-    typeCheck: true
-  },
-  modules: [
-    'vuetify-nuxt-module',
-    '@nuxtjs/tailwindcss'
-  ],
-  vuetify: {
-    moduleOptions: {
-      /* module specific options */
-    },
-    vuetifyOptions: {
-      /* vuetify options */
-    }
-  }
+  devtools: { enabled: true }
 })
